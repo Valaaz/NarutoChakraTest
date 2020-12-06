87c3a361631de1efe2f4fdd393bf084830ab1b6a
Valentin AZANCOTH <62429527+Valaaz@users.noreply.github.com>
2020-12-06 14:31:58 +0100

    added button start and questions number

---
+++ js/questions.js
@@ -0,0 +1,18 @@
+function questions()
+{
+    var question = "";
+    switch(number)
+    {
+        case 1:
+            question = "Naruto";
+            break;
+        case 2:
+            question = "Sasuke";
+            break;
+        case 3:
+            question = "Sakura";
+            break;
+    }
+
+    return question;
+}
